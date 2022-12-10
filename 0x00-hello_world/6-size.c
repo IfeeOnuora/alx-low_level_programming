155aab8e86a5442bf373380b2a9b70698f8b794e
Ifechukwu Onuora <onyinyeonuora@gmail.com>
2022-12-10 04:26:12 -0800

    size is not gradeur, and territory does not make a nation

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float f;
+
+	printf("Size of a char: %lu byte(S)\n", (unsigned long)sizeof(a));
+	printf("Size of an int: %lu byte(S)\n", (unsigned long)sizeof(b));
+	printf("size of a long int: %lu byte(S)\n", (unsigned long)sizeof(c));
+	printf("size of a long long int: %lu byte(S)\n", (unsigned long)sizeof(d));
+	printf("size of a float: %lu byte(S)\n", (unsigned long)sizeof(f));
+	return (0);
+}
