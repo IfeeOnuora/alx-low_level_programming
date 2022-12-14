c3cf0a809dc465cb88e1da27fa96946befdf2f7a
Ifechukwu Onuora <onyinyeonuora@gmail.com>
2022-12-14 06:00:12 -0800

    _is alpha

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -2,11 +2,12 @@
 /**
  * _isalpha - checks for char alphabet
  * @c: the char to be checked
- * Return: 1 if char is letter lower or upper, otherwise 0
+ *
+ * Return: 1 if char is letter, lower or upper, otherwise 0
  */
 int _isalpha(int c)
 {
-	if ((char c >= 'a' && c <= 'z') || (char c >= 'A' && c <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 		return (1);
 	else
 		return (0);
