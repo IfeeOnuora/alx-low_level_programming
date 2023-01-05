00b459e3a323be54eeb9a7ca294eea81d8ad11b2
Ifechukwu Onuora <onyinyeonuora@gmail.com>
2023-01-05 05:19:54 -0800

    0-recursion

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -7,7 +7,6 @@
  */
 void _puts_recursion(char *s)
 {
-	
 	if (*s == '\0')
 	{
 		_putchar('\n');
@@ -17,4 +16,5 @@ void _puts_recursion(char *s)
 		_putchar(*s);
 		_puts_recursion(s + 1);
 	}
-}	
+
+}
