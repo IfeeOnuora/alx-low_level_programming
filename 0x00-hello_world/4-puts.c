76199100137215bbe1ae7a3d10665099a9acab34
Ifee Onuora <onyinyeonuora@gmail.com>
2023-03-16 04:38:00 -0700

    puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * This program is to print a string using puts
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle\n");
+
+	return (0);
+}
