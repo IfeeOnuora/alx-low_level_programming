a2fdadd2699b2c8ad3af734144e0241a4c4c6901
Ifee Onuora <onyinyeonuora@gmail.com>
2023-03-16 04:58:50 -0700

    Hello, printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - writes a program that prints a string
+ *
+ * @printf - the function used to print the string
+ *
+ * Return: 0 on success
+ */
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+
+	return (0);
+}
