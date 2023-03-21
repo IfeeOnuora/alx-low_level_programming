e5ee0cec2d48c1ecedfa590b6c1685a69fa59232
Ifee Onuora <onyinyeonuora@gmail.com>
2023-03-21 04:36:24 -0700

    alphabet

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - write a program that prints alphabet in lower case
+ *
+ * Return: 0 is sucess
+ */
+int main(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		putchar(i);
+	putchar("\n");
+
+	return (0);
+}
