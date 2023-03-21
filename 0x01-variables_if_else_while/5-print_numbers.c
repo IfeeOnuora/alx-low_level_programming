a1676f0bcdb1003712fb7e5e227342d0de83d5bd
Ifee Onuora <onyinyeonuora@gmail.com>
2023-03-21 05:23:08 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - prints numbers from 0-10
+ *
+ * Return: 0 if successful
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10; a++)
+		printf("%d", a);
+	printf("\n");
+
+	return (0);
+}
