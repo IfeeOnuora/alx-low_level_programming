0e2dd3dfd2685f6f1b72054adbd29f8d2c8d7ff6
Ifee Onuora <onyinyeonuora@gmail.com>
2023-03-21 06:26:21 -0700

    hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - prints hexadecimal numbers in lowercase
+ *
+ * Return: 0 when successful
+ */
+
+int main(void)
+{
+	int i;
+	char j;
+
+	for (i = 0; i <= 9; i++)
+		putchar(i + '0');
+	for (j = 'a'; j <= 'e'; j++)
+	       putchar(j);
+	putchar('\n');
+	
+	return (0);
+}	
