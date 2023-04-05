f49203d658bd8923f68ad14e2dd3bc6439aa1ef1
Ifee Onuora <onyinyeonuora@gmail.com>
2023-04-05 01:26:01 -0700

    pointer update

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ * reset_to_9 - a function that takes a pointer to an int
+ * as parameter and updates the value to 98
+ *
+ * @n: the parameter to be updated
+ *
+ * Return: 0.
+ */
+void reset_to_98(int *n)
+{
+	*n = 98;
+}
