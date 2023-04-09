ea5504f51bd07d08bd022228c77137f1484fdfb2
Ifee Onuora <onyinyeonuora@gmail.com>
2023-04-09 19:03:30 +0000

    reverse string

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_rev - function that prints a string in reverse
+ *
+ * @s: function parameter
+ *
+ * Return: 0.
+ */
+void print_rev(char *s)
+{
+	int j, c = 0;
+
+	for (j = 0; s[j] != '\0'; j++)
+		c++;
+	for (j = c; s[j] >= 0; j--)
+		_putchar(s[j]);
+	_putchar('\n');
+}	
