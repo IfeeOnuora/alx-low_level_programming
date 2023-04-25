8a0cc9a5c1b9fd6d4e0d488b03ff1feca2d71730
Ifee Onuora <onyinyeonuora@gmail.com>
2023-04-25 13:39:11 +0000

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strncat - a function that concatenates strings
+ * without using strncat
+ *
+ * @dest: the destination 
+ * @src: the source
+ * @n: the number needed to append
+ *
+ * Return: dest.
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int x, y;
+
+	x = 0;
+	while(dest[x] != '\0')
+	{
+		x++;
+	}
+	y = 0;
+	while(src[y] != '\0' && y < n)
+	{
+		dest[x] = src[y];
+		x++;
+		y++;
+	}
+	return(dest);
+}
