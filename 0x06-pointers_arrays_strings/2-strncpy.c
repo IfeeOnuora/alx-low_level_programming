be201c443dcbb665651d6e1d70bbd79002e2e267
Ifee Onuora <onyinyeonuora@gmail.com>
2023-04-25 14:34:59 +0000

    strncpy

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -13,12 +13,12 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int x = 0;
 
-	while (x < n && src[x])
+	while (x < n && *(src + x))
 	{
-		dest[x] = src[x];
+		*(dest + x) = *(src + x);
 		x++;
 	}
-	dest[x] = '\0';
+	*(dest + x) = '\0';
 	return (dest);
 }
 
