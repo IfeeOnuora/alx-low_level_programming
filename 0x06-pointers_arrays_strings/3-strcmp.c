a6a4f720562608187bed70c8af5a1eaac9b36e45
Ifee Onuora <onyinyeonuora@gmail.com>
2023-04-26 12:04:44 +0000

    string compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,17 +11,13 @@ int _strcmp(char *s1, char *s2)
 {
 	int x;
 
-	for (x = 0; s1[x] != '\0' || s2[x] != '\0'; x++)
+	for (x = 0; s1[x] != '\0' && s2[x] != '\0'; x++)
 	{
 		if (s1[x] != s2[x])
 		{
-			if (s1[x] < s2[x])
-				return (s1[x] - s2[x]);
-			else if (s2[x] < s1[x])
-				return (s1[x] - s2[x]);
+			break;
 		}
-		else
-			return (0);
+		return (s1[x] - s2[x]);
 	}
 	return (0);
 }
