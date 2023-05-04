7d43a9b87df608d9d8dc9a64e40cf6c2d663d77a
Ifee Onuora <onyinyeonuora@gmail.com>
2023-05-04 15:18:50 +0000

    reverse string

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -11,10 +11,15 @@ void rev_string(char *s)
 {
 	int i;
 	int c = 0;
+	char y;
 
 	for (i = 0; s[i] != '\0'; i++)
 		c++;
-	i = c - 1;
-	while(i >= 0)
-		i--;
+	for (i = c - 1; i >= c / 2; i--)
+	{
+		y = s[i];
+		s[i] = s[c - 1 - i];
+		s[c - 1 - i] = y;
+	}
+
 }
