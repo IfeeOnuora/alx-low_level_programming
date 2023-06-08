aa1caaa0f105c5267a530948b9547f5deeacaf5d
Ifee Onuora <onyinyeonuora@gmail.com>
2023-06-08 12:38:51 +0000

    Recursion

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strlen_recursion - a function that returns the function of
+ * a string.
+ *
+ * @s: the parameter passed to the function.
+ *
+ * Return: 0 when successful.
+ */
+int _strlen_recursion(char *s)
+{
+	int count = 1;
+
+	if (*s++)
+		count += _strlen_recursion(s);
+	else
+		count --;
+	return (count);
+}
